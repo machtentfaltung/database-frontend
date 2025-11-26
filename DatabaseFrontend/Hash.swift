29cf1ac1df8cf17e972f828567bda02b35f5a5d4
Matei Pralea <matei@pralea.me>
2025-11-26 20:19:35 +0200

    feat: initial commit

---
+++ DatabaseFrontend/Hash.swift
@@ -0,0 +1,30 @@
+//  SPDX-License-Identifier: MIT OR Apache-2.0
+//
+//  Hash.swift
+//  DatabaseFrontend
+//
+//  Created by Matei Pralea on 22/04/2024.
+//  Copyright © 2024-2025 Matei Pralea. All rights reserved.
+//
+
+import Foundation
+import CryptoKit
+
+
+func computeHash(for string: String, salt: String = String()) -> String {
+    if salt.isEmpty {
+        return SHA512.hash(data: Data(string.utf8))
+            .compactMap { String(format: "%02x", $0) }
+            .joined()
+    }
+    else {
+        return SHA512.hash(data: Data("\(string)\(salt)".utf8))
+            .compactMap { String(format: "%02x", $0) }
+            .joined()
+    }
+}
+
+func salt(length: Int = 12) -> String {
+    let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    return String((0..<length).map{ _ in letters.randomElement()! })
+}
